01916a1542d3e2a97219930cf0e0ed472c7de3cc
saaviGit <chaminisavindya95@gmail.com>
2019-05-19 22:23:31 +0530

    deleted

--- FoodItemSellNBye/src/main/java/com/food/Repository/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.food.Repository;
-
-import com.food.model.item;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public interface ItemRepository extends JpaRepository<item, Integer>{
-	
-}
